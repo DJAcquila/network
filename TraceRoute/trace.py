ba4abd9390518a1f2d4e62c696fec382284f8f15
DJAcquila <acqila.santos@gmail.com>
2018-04-09 20:22:12 -0300

    Trace modifications

--- TraceRoute/trace.py
+++ TraceRoute/trace.py
@@ -6,14 +6,17 @@ import sys
 import struct
 import time	
 
-class flushfile(file):
+class flush(file):
     def __init__(self, f):
+
         self.f = f
+
     def write(self, x):
+
         self.f.write(x)
+
         self.f.flush()
 
-sys.stdout = flushfile(sys.stdout)
 
 def main():
 	d_url = sys.argv[1]
@@ -32,6 +35,7 @@ def main():
 
 		recv = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname('icmp'))
 		send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.getprotobyname('udp'))
+		
 		#Set the value of the given socket option
 		send.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 
@@ -43,40 +47,57 @@ def main():
 		t = (time.time())
 
 		sys.stdout.write(" %d \t" % ttl)
+
 		send.sendto("",(d_url, port))
 
 		flag = False
 		recv_addr = None
 		recv_name = None
-
-		
+		i = 0
+		flag1 = False
+		rt = []
+		while not flag and i < 3:
 			
-		try:
-			_, c_addr = recv.recvfrom(512)
+			try:
+				_, c_addr = recv.recvfrom(512)
 
-			roundt = ((time.time()) - t) * 1000
-			#sys.stdout.write("Teste: %s\n" % c_addr[0])
-			
-			c_addr = c_addr[0]
-			
-			c_name = socket.gethostbyaddr(c_addr)[0]
+				roundt = ((time.time()) - t) * 1000
+				rt.append(roundt)
+				#sys.stdout.write("Teste: %s\n" % c_addr[0])
+				c_addr = c_addr[0]
+				
+				try:
+				
+					c_name = socket.gethostbyaddr(c_addr)[0]
 
-			flag = True
+				except:
+					c_name = c_addr
 
-		except socket.error:
-			
-			sys.stdout.write("* * *")
+				flag = True
+
+			except socket.error:
+				i = i + 1	
+				sys.stdout.write("* ")
+				flag1 = True
+
+		if flag1:
 			sys.stdout.write("\n")
-						
+					
 		send.close()
 		recv.close()
 		
 		if c_addr is not None and flag is not False:
-			sys.stdout.write("(%s)  %s  %.3f ms\n" %(c_addr, c_name, roundt))
 
+			sys.stdout.write("(%s)  %s " %(c_addr, c_name))
+			for x in range(len(rt)):
+				sys.stdout.write("%.3f ms " % rt[x])
+
+			sys.stdout.write("\n")
 		ttl = ttl + 1
 
 		if (c_addr == d_addr): 
 			break
+
+sys.stdout = flush(sys.stdout)
 
 main()
